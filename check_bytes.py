c68dc986318809988dcfc3f67764c9b93b05c661
Ibai Montero <ibai.vilches@tprs.stud.vu.lt>
2026-04-24 21:44:09 +0200

    Critical: Remove purge_fix.js (Node.js require breaks GAS), strip ALL non-ASCII from script blocks, add .claspignore

--- check_bytes.py
+++
@@ -1,5 +0,0 @@
-
-with open(r"c:\Users\elzorro1\OneDrive\Escritorio\Nueva carpeta\SoloQ_Challenge\code.gs", "rb") as f:
-    lines = f.readlines()
-    for i in range(60, 68):
-        print(f"Line {i+1}: {lines[i]}")
